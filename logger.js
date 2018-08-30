1c4ab26a23ba6bb4ae86693cd3393f910a72ca2b
Tushar <tusharnimje21@gmail.com>
2018-08-30 21:35:50 +0530

    deleted min file (not required) added isArray function created logger.js for logging

---
+++ logger.js
@@ -0,0 +1,29 @@
+export default class Logger {
+  constructor() {
+    this.info = 1;
+    this.warn = 2;
+    this.error = 3;
+    this.disable = 4;
+  }
+
+  _noop() {}
+
+  setlevel(key = this.info) {
+    if (!key) {
+      throw "Logger level is required";
+    }
+
+    switch (key) {
+      case this.disable:
+        console.error = this._noop;
+      case this.error:
+        console.warn = this._noop;
+      case this.warn:
+        console.log = this._noop;
+      case this.info:
+        break;
+      default:
+        throw "invalid choice";
+    }
+  }
+}
