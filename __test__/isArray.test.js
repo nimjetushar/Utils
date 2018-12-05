5f9ba942b24baa48cf544f9d8de740bfff3174a3
tusharnimje21@gmail.com <tusharnimje21@gmail.com>
2018-12-05 22:12:10 +0530

    added isArray.test.js

---
+++ __test__/isArray.test.js
@@ -0,0 +1,30 @@
+import { isArray } from "../core/isArray";
+
+describe("isArray", () => {
+  it("should return a function", () => {
+    expect(isArray).toBeDefined();
+
+    const type = typeof isArray;
+    expect(type).toBe("function");
+  });
+
+  it("should identify passed variable is array or not", () => {
+    let isDtArray = isArray([]);
+    expect(isDtArray).toBeTruthy();
+
+    isDtArray = isArray([1, 2, "it"]);
+    expect(isDtArray).toBeTruthy();
+
+    isDtArray = isArray({ it: "it" });
+    expect(isDtArray).toBeFalsy();
+
+    isDtArray = isArray(1);
+    expect(isDtArray).toBeFalsy();
+
+    isDtArray = isArray("it string");
+    expect(isDtArray).toBeFalsy();
+
+    isDtArray = isArray(new Date());
+    expect(isDtArray).toBeFalsy();
+  });
+});
