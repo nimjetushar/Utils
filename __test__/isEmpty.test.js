16c3c275fcc054c45abafa13f93b05277d6a46f3
tusharnimje21@gmail.com <tusharnimje21@gmail.com>
2018-12-13 20:37:59 +0530

    added isEmpty.test.js

---
+++ __test__/isEmpty.test.js
@@ -0,0 +1,9 @@
+import { isEmptyObject } from "../core/isEmpty";
+
+describe("isEmpty", () => {
+  it("should check for empty object", () => {
+    expect(isEmptyObject({})).toBeTruthy();
+
+    expect(isEmptyObject({ it: "it" })).toBeFalsy();
+  });
+});
