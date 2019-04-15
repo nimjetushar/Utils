f68295832e75670167b4383993d710ad90fe35de
tusharnimje21@gmail.com <tusharnimje21@gmail.com>
2019-04-15 23:12:50 +0530

    updated deepCopy.test.js

--- __test__/deepCopy.test.js
+++ __test__/deepCopy.test.js
@@ -1,4 +1,5 @@
 import { deepCopy } from "../core/deepClone";
+import { isEqual } from "../core/isEqual";
 
 describe("deepCopy", () => {
   it("should verify variable to be deep copy", () => {
@@ -9,5 +10,30 @@ describe("deepCopy", () => {
     clonedObj.id = 100;
     const isChanged = data.id !== clonedObj.id;
     expect(isChanged).toBeTruthy();
+  });
+
+  it("should copy complex variable", () => {
+    const data = { id: 123, name: ["a", "v", "d"], obj: { a: 299, list: [1, 2, 3, 4, 34, 35, 75] } };
+    const clonedObj = deepCopy({}, data);
+    expect(data).toEqual(clonedObj);
+
+    clonedObj.id = 100;
+    clonedObj.name.push("t");
+    let isChanged = data.id !== clonedObj.id;
+    expect(isChanged).toBeTruthy();
+
+    isChanged = !isEqual(clonedObj.name, data.name);
+    expect(isChanged).toBeTruthy();
+  });
+
+  it("should copy object proto", () => {
+    function Demo() { }
+    Demo.prototype.getValue = function () { return 100; };
+
+    const obj = new Demo();
+    const clonedObj = deepCopy({}, obj);
+
+    expect(clonedObj.getValue).toBeDefined();
+    expect(clonedObj.getValue()).toBe(100);
   });
 });
