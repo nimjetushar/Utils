124196fa7f6c0bedf088122aa7e2a4f59b20269b
Tushar <tusharnimje21@gmail.com>
2020-06-14 00:37:38 +0530

    added compiled file

---
+++ build/logger.js
@@ -0,0 +1,182 @@
+/**
+* @author Tushar Nimje
+* @description Utility functions
+*/
+/* eslint-disable */
+module.exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, "default", function() { return /* binding */ logger_Logger; });
+
+// CONCATENATED MODULE: ./core/noop.js
+/**
+ * noop
+ * @param {*} value accepts anyumber of arguments.
+ * @returns {void} return undefined
+ */
+function noop() {}
+// CONCATENATED MODULE: ./logger.js
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+
+/**
+ * Enable / disable logging across various environment
+ * new Logger().setlevel(level) where level are :-
+ * - 1 -> All console methods are enabled.
+ * - 2 -> console log and info methods are diabled while other works as expected
+ * - 3 -> console.error method will be enabled while other are disabled
+ * - 4 -> All console methods are disabled
+ * @export
+ * @class Logger
+ */
+
+var logger_Logger = /*#__PURE__*/function () {
+  function Logger() {
+    _classCallCheck(this, Logger);
+
+    this.info = 1;
+    this.warn = 2;
+    this.error = 3;
+    this.disable = 4;
+  }
+  /**
+   * Set logger level
+   * @param {number} [key=this.info] logging level
+   * @returns {void}
+   * @memberof Logger#
+   */
+
+
+  _createClass(Logger, [{
+    key: "setlevel",
+    value: function setlevel() {
+      var key = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : this.info;
+
+      if (!key) {
+        throw new Error('Logger level is required');
+      }
+
+      switch (key) {
+        case this.disable:
+          console.error = noop;
+
+        case this.error:
+          console.warn = noop;
+
+        case this.warn:
+          console.info = noop;
+          console.log = noop;
+
+        case this.info:
+          break;
+
+        default:
+          throw new Error('invalid choice');
+      }
+    }
+  }]);
+
+  return Logger;
+}();
+
+
+
+/***/ })
+/******/ ]);
--- gulpfile.js
+++ gulpfile.js
@@ -9,19 +9,26 @@ function createDoc(cb) {
 }
 
 function build(cb) {
-    return run('npm run build-util-umd').exec()
-        .pipe(run('npm run build-detect-umd'))
-        .pipe(run('npm run build-logger-umd'))
+    return run('npm run build-util').exec()
+        .pipe(run('npm run build-detect'))
+        .pipe(run('npm run build-logger'))
         .pipe(run('echo build executed', cb()));
 }
 
-function buildMin(cb) {
+function buildUmd(cb) {
+    return run('npm run build-util-umd').exec()
+        .pipe(run('npm run build-detect-umd'))
+        .pipe(run('npm run build-logger-umd'))
+        .pipe(run('echo build umd executed', cb()));
+}
+
+function buildUmdMin(cb) {
     return run('npm run build-util-umd:min').exec()
         .pipe(run('npm run build-detect-umd:min'))
         .pipe(run('npm run build-logger-umd:min'))
         .pipe(run('echo build min executed', cb()));
 }
 
-exports['build:min'] = gulp.series(buildMin);
-exports.build = gulp.series(build);
+exports['build:min'] = gulp.series(buildUmdMin);
+exports.build = gulp.parallel(build, buildUmd);
 exports.default = gulp.series(createDoc);
